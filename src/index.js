b2fc0d0fa5364643317e0b194e2bfeab35719baf
academeg1 <m-ignatovich@hotmail.com>
2022-07-01 11:59:55 +0300

    feat: done work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let mass = String(n).split('')
+    if (mass[0] === '-') {
+        mass.shift()
+    }
+    mass = mass.reverse();
+    mass = Number(mass.join(''))
+
+        return mass
 }
